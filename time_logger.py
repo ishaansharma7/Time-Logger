1e154a1d380ebd12f606fe894ca7f67ef21d34c6
ishaansharma7 <ishaansharma711@gmail.com>
2021-06-17 23:59:19 +0530

    update

--- time_logger.py
+++ time_logger.py
@@ -6,13 +6,15 @@ from datetime import datetime
 def time_logger(location='generic-logs'):
     
     def my_timer(orig_func):
-        final_location = os.path.join('logs', location)
-        if os.path.exists(final_location) != True:
-            print('Directory does not exist, creating directory')
-            os.makedirs(final_location)
         
         @wraps(orig_func)
         def wrapper(*args, **kwargs):
+            
+            final_location = os.path.join('logs', location)
+            if os.path.exists(final_location) != True:
+                print('Directory does not exist, creating directory')
+                os.makedirs(final_location)
+            
             logging.basicConfig(filename=f'{final_location}/{orig_func.__name__}.log', level=logging.INFO, force=True)
             starting_time = datetime.now()
             t1 = datetime.today().timestamp()
@@ -20,6 +22,7 @@ def time_logger(location='generic-logs'):
             ended_time = datetime.now()
             t2 = datetime.today().timestamp() - t1
             logging.info(f'{orig_func.__name__} ran in {round(t2, 3)} seconds on {starting_time}, ended at {ended_time}')
+            
             return result  
         return wrapper
     return my_timer
